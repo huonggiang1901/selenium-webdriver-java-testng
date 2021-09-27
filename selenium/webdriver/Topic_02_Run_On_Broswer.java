db8b0d1024e7be5d87b13b75228b3d29dc1858de
Giang Cong Huyen <huonggiang190195@gmail.com>
2021-09-27 18:11:55 +0700

    Add Topic_02

---
+++ selenium/webdriver/Topic_02_Run_On_Broswer.java
@@ -0,0 +1,40 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic_02_Run_On_Broswer {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@Test
+	public void TC_01_Firefox_Lastest() {
+		// Firefox lastest: 89
+		// Selenum 3.141.59
+		// TestNG 6.14.3
+		// Gecko Driver
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+		driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+	
+		driver.quit();
+	}
+	
+	//@Test
+	public void TC_02_Firefox_Old() {
+		// Firefox 47.0.2
+		// Selenium 2.53.1
+		// Ko TestNG
+		// Ko Gecko Driver
+		driver = new FirefoxDriver();
+		
+		driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+		
+		driver.quit();
+	}
+
+		
+
+}
