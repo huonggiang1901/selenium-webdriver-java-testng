4caaa80a66a96659158c973d84422e2d2f73a6f2
Giang Cong Huyen <huonggiang190195@gmail.com>
2021-09-27 18:11:39 +0700

    Update Topic_01

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -17,13 +17,13 @@ public class Topic_01_Check_Environment {
 
 	@BeforeClass
 	public void beforeClass() {
-		
-				System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-				driver = new FirefoxDriver();
-		
+
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
 //		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 //		driver = new ChromeDriver();
-		
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/v4/");
@@ -31,7 +31,7 @@ public class Topic_01_Check_Environment {
 
 	@Test
 	public void TC_01_ValidateCurrentUrl() {
-		
+
 		// Login Page Url matching
 		String loginPageUrl = driver.getCurrentUrl();
 		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
@@ -39,7 +39,7 @@ public class Topic_01_Check_Environment {
 
 	@Test
 	public void TC_02_ValidatePageTitle() {
-		
+
 		// Login Page title
 		String loginPageTitle = driver.getTitle();
 		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
@@ -47,7 +47,7 @@ public class Topic_01_Check_Environment {
 
 	@Test
 	public void TC_03_LoginFormDisplayed() {
-		
+
 		// Login form displayed
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
 	}
