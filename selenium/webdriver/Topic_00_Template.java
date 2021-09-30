72790922f3ae2cd8131b1f6d21dbdcd227d0d47a
Giang Cong Huyen <huonggiang190195@gmail.com>
2021-09-30 20:47:56 +0700

    Add Topic 00 Template

---
+++ selenium/webdriver/Topic_00_Template.java
@@ -0,0 +1,32 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	
+	@BeforeClass
+	public void BeforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void TC() {
+		
+	}
+	
+	@AfterClass
+	public void AfterClass() {
+		driver.quit();
+	}
+}
