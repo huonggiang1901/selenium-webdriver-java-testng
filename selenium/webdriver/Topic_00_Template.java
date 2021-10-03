1468ca06a1c6faa19619e30f57186f03d5991e3e
Giang Cong Huyen <huonggiang190195@gmail.com>
2021-10-03 09:28:25 +0700

    Update Template Add Topic 07 Custom_Textbox_Textarea

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -1,9 +1,11 @@
 package webdriver;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,6 +20,7 @@ public class Topic_00_Template {
 
 	String projectPath = System.getProperty("user.dir");
 	
+	
 	@BeforeClass
 	public void BeforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
@@ -30,7 +33,8 @@ public class Topic_00_Template {
 	
 	@Test
 	public void TC() {
-		
+		WebElement element = driver.findElement(By.xpath(""));
+		js.executeScript("arguments[0].scrollIntoView();", element);
 	}
 	
 	@AfterClass
@@ -54,5 +58,50 @@ public class Topic_00_Template {
 	
 	public void scrollIntoView(WebElement element) {
 		js.executeScript("arguments[0].scrollIntoView();", element);
+	}
+	
+	public boolean isElementDisplayed(By locator) {
+		if (driver.findElement(locator).isDisplayed()) {
+			System.out.println(locator + " is displayed ");
+			return true;
+		} else
+			System.out.println(locator + " is not displayed ");
+		return false;
+	}
+
+	public boolean isElementEnabled(By locator) {
+		if (driver.findElement(locator).isEnabled()) {
+			System.out.println(locator + " is enabled ");
+			return true;
+		} else
+			System.out.println(locator + " is disabled ");
+		return false;
+	}
+
+	public boolean isElementSelected(By locator) {
+		if (driver.findElement(locator).isSelected()) {
+			System.out.println(locator + " is selected ");
+			return true;
+		} else
+			System.out.println(locator + " is deselected ");
+		return false;
+	}
+
+	public void sendKeysToElement(By locator, String value) {
+		WebElement element = driver.findElement(locator);
+		element.clear();
+		element.sendKeys(value);
+	}
+
+	public void clickElement(By locator) {
+		driver.findElement(locator).click();
+	}
+	
+	public String formatDateFromString(String date, String oldFormat, String newFormat) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(oldFormat);
+		Date d = sdf.parse(date);
+		sdf.applyPattern(newFormat);		
+		String newDateString = sdf.format(d);
+		return newDateString;
 	}
 }
