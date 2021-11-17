5ea4eb1195eee25e83fdf5e892c8328387e4d038
Giang Cong Huyen <huonggiang190195@gmail.com>
2021-11-17 17:45:20 +0700

    Add RunAdminMode.xml Add RunAll.xml Add RunTestcases.xml Add RunUserMode.xml Add Topic 05 Data provider Add Topic 06 Parameter Add Topic 07 Loop Add Topic 08 Dependence

---
+++ selenium/testng/Topic_08_Dependence.java
@@ -0,0 +1,27 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Topic_08_Dependence {
+
+	@Test
+	public void TC_01_Create_Account() {
+		System.out.println("Run TC_01_Create_Account");
+	}
+
+	@Test(dependsOnMethods = "TC_01_Create_Account")
+	public void TC_02_Edit_Account() {
+		System.out.println("Run TC_02_Edit_Account");
+	}
+
+	@Test(dependsOnMethods = { "TC_01_Create_Account", "TC_02_Edit_Account" })
+	public void TC_03_View_Account() {
+		System.out.println("Run TC_03_View_Account");
+	}
+
+	@Test(dependsOnMethods = "TC_01_Create_Account")
+	public void TC_04_Delete_Account() {
+		System.out.println("Run TC_04_Delete_Account");
+	}
+
+}
